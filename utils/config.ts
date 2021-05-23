b52057776c4bc3abfe94d07332a304445d3f72cf
Aryan Misra <aryanmisra4@gmail.com>
2021-05-22 20:53:33 -0400

    config updates

--- utils/config.ts
+++ utils/config.ts
@@ -5,7 +5,6 @@ export const MONGODB_URI = process.env.MONGODB_URI;
 export const MAIL = process.env.MAIL; // support email password
 export const SECRET = process.env.SECRET; //jwt secret
 export const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY; //etherscan api key
-export const CRYPTOCOMPARE_KEY = process.env.CRYPTOCOMPARE_KEY; //cryptocompare api key
 export const ADDRESS_ENCRYPT_PRIVATEKEY = process.env.ADDRESS_ENCRYPT_PRIVATEKEY ? process.env.ADDRESS_ENCRYPT_PRIVATEKEY : ""; //pool encryption
 export const HttpProvider = process.env.HttpProvider; //alchemy http provider url
 export const XAlchemyToken = process.env.XAlchemyToken; //alchemy auth token
@@ -15,8 +14,10 @@ export const PUBLIC_KEY_BITCLOUT = process.env.PUBLIC_KEY_BITCLOUT; //escrow wal
 export const SEED_HEX = process.env.SEED_HEX; //bitclout seed hex for identity
 export const cfuid = "d948f4d42aa8cf1c00b7f93ba8951d45b1619496624";
 export const ingressCookie = "c7d7d1526f37eb58ae5a7a5f87b91d24";
-export const WEBHOOK_ID = 148926;
-export const NETWORK = "kovan";
-export const EXCHANGE_API = "http://localhost:5050";
-export const ROLLBAR = process.env.ROLLBAR;
+export const WEBHOOK_ID = process.env.ENVIRONMENT === "production" ? 149131 : 149102;
+export const NETWORK = process.env.ENVIRONMENT === "production" ? "mainnet" : "kovan";
+export const EXCHANGE_API =
+  process.env.ENVIRONMENT === "production"
+    ? "https://bitswap-exchange-api-prod.herokuapp.com/"
+    : "https://bitswap-exchange-api-staging.herokuapp.com/";
 export const MinFeeRateNanosPerKB = 1000;
