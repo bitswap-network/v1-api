e1aca4f65b7e0c72488ccf75af40e30e4076c2b3
Aryan Misra <aryanmisra4@gmail.com>
2021-05-25 03:23:25 -0400

    Update users.ts

--- controllers/users.ts
+++ controllers/users.ts
@@ -83,7 +83,7 @@ userRouter.get("/verify-email/:code", async (req, res, next) => {
 });
 
 userRouter.get("/verify-bitclout/:depth", tokenAuthenticator, async (req, res, next) => {
-  const user = await User.findOne({ username: req.user.username }).exec();
+  const user = await User.findOne({ "bitclout.publicKey": req.key });
   const numToFetch = Number(req.params.depth) ? Number(req.params.depth) : 10;
 
   if (user) {
