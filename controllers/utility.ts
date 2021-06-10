0ad90a0756422b27fe0c551039e4156bd85890c3
Aryan Misra <aryanmisra4@gmail.com>
2021-06-10 19:45:14 -0400

    Update utility.ts

--- controllers/utility.ts
+++ controllers/utility.ts
@@ -142,27 +142,36 @@ utilRouter.get("/order-history", async (req, res, next) => {
       ? `${orders[0].completeTime.getFullYear()}-${orders[0].completeTime.getMonth()}-${orders[0].completeTime.getDate()}`
       : `${orders[0].created.getFullYear()}-${orders[0].created.getMonth()}-${orders[0].created.getDate()}`;
     let dateString2 = "";
-    let priceSum = orders[0].orderPrice;
+    let priceQuantitySum =
+      (orders[0].execPrice ? orders[0].execPrice : orders[0].orderPrice) *
+      (orders[0].orderQuantityProcessed ? orders[0].orderQuantityProcessed : orders[0].orderQuantity);
     let quantSum = orders[0].orderQuantityProcessed ? orders[0].orderQuantityProcessed : orders[0].orderQuantity;
     for (let i = 1; i < orders.length; ++i) {
       if (orders[i]) {
-        const orderPrice = orders[i].orderPrice;
+        const orderPriceQuantity =
+          (orders[i].execPrice ? orders[i].execPrice! : orders[i].orderPrice) *
+          (orders[i].orderQuantityProcessed ? orders[i].orderQuantityProcessed : orders[i].orderQuantity);
         const orderQuantity = orders[i].orderQuantityProcessed ? orders[i].orderQuantityProcessed : orders[i].orderQuantity;
+
         dateString2 = orders[i].completeTime
           ? `${orders[i].completeTime!.getFullYear()}-${orders[i].completeTime!.getMonth()}-${orders[i].completeTime!.getDate()}`
           : `${orders[i].created.getFullYear()}-${orders[i].created.getMonth()}-${orders[i].created.getDate()}`;
-        if (dateString1 === dateString2 && orderQuantity && orderPrice) {
-          priceSum += orderPrice;
+        if (dateString1 === dateString2 && orderQuantity && orderPriceQuantity && priceQuantitySum && quantSum) {
+          priceQuantitySum += orderPriceQuantity;
           quantSum += orderQuantity;
-        } else if (orderQuantity && orderPrice) {
-          finalArr.push({ timestamp: new Date(dateString1), price: Math.round((priceSum / quantSum + Number.EPSILON) * 100) / 100 });
-          priceSum = orderPrice;
+        } else if (orderQuantity && orderPriceQuantity && priceQuantitySum && quantSum) {
+          finalArr.push({
+            timestamp: new Date(dateString1),
+            price: Math.round((priceQuantitySum / quantSum + Number.EPSILON) * 100) / 100,
+          });
+
+          priceQuantitySum = orderPriceQuantity;
           quantSum = orderQuantity;
         }
         dateString1 = dateString2;
       }
     }
-    finalArr.push({ timestamp: new Date(dateString1), price: Math.round((priceSum / quantSum + Number.EPSILON) * 100) / 100 });
+    finalArr.push({ timestamp: new Date(dateString1), price: Math.round((priceQuantitySum / quantSum + Number.EPSILON) * 100) / 100 });
     res.json(finalArr);
   } catch (e) {
     next(e);
