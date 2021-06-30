3fcd53a92867176715e7ca58f85b9daa3780c80d
Aryan Misra <aryanmisra4@gmail.com>
2021-06-29 20:57:27 -0400

    linting

--- controllers/utility.ts
+++ controllers/utility.ts
@@ -1,5 +1,5 @@
-import {getGasEtherscan, getEthUsd, getOrderbookState, getBitcloutUsd} from "../utils/functions";
-import Depth, {depthDoc} from "../models/depth";
+import { getGasEtherscan, getEthUsd, getOrderbookState, getBitcloutUsd } from "../utils/functions";
+import Depth, { depthDoc } from "../models/depth";
 import Order from "../models/order";
 //TODO CLEANUP
 const utilRouter = require("express").Router();
@@ -7,7 +7,7 @@ const utilRouter = require("express").Router();
 utilRouter.get("/eth-gasprice", async (req, res, next) => {
   try {
     const response = await getGasEtherscan();
-    res.json({data: response.data.result});
+    res.json({ data: response.data.result });
   } catch (e) {
     next(e);
   }
@@ -16,7 +16,7 @@ utilRouter.get("/eth-gasprice", async (req, res, next) => {
 utilRouter.get("/eth-usd", async (req, res, next) => {
   try {
     const response = await getEthUsd();
-    res.json({data: response});
+    res.json({ data: response });
   } catch (e) {
     next(e);
   }
@@ -25,7 +25,7 @@ utilRouter.get("/eth-usd", async (req, res, next) => {
 utilRouter.get("/bitclout-usd", async (req, res, next) => {
   try {
     const bitcloutUsd = await getBitcloutUsd();
-    res.json({data: +bitcloutUsd.toFixed(2)});
+    res.json({ data: +bitcloutUsd.toFixed(2) });
   } catch (e) {
     next(e);
   }
@@ -45,13 +45,13 @@ utilRouter.get("/order-history", async (req, res, next) => {
     const orders = await Order.find({
       complete: true,
       error: "",
-      orderPrice: {$gt: 100},
-      orderQuantityProcessed: {$gt: 0},
+      orderPrice: { $gt: 100 },
+      orderQuantityProcessed: { $gt: 0 },
     })
-      .sort({completeTime: "desc"})
+      .sort({ completeTime: "desc" })
       .exec();
     if (orders.length > 0) {
-      const finalArr: {timestamp: Date; price: number}[] = [];
+      const finalArr: { timestamp: Date; price: number }[] = [];
       let dateString1 = orders[0].completeTime
         ? `${orders[0].completeTime.getUTCFullYear()}-${orders[0].completeTime.getUTCMonth() + 1}-${orders[0].completeTime.getUTCDate()}`
         : `${orders[0].created.getUTCFullYear()}-${orders[0].created.getUTCMonth() + 1}-${orders[0].created.getUTCDate()}`;
@@ -67,8 +67,8 @@ utilRouter.get("/order-history", async (req, res, next) => {
 
           dateString2 = orders[i].completeTime
             ? `${orders[i].completeTime!.getUTCFullYear()}-${orders[i].completeTime!.getUTCMonth() + 1}-${orders[
-              i
-            ].completeTime!.getUTCDate()}`
+                i
+              ].completeTime!.getUTCDate()}`
             : `${orders[i].created.getUTCFullYear()}-${orders[i].created.getUTCMonth() + 1}-${orders[i].created.getUTCDate()}`;
           if (dateString1 === dateString2 && orderQuantity && orderPriceQuantity && priceQuantitySum && quantSum) {
             priceQuantitySum += orderPriceQuantity;
@@ -85,7 +85,7 @@ utilRouter.get("/order-history", async (req, res, next) => {
           dateString1 = dateString2;
         }
       }
-      finalArr.push({timestamp: new Date(dateString1), price: Math.round((priceQuantitySum / quantSum + Number.EPSILON) * 100) / 100});
+      finalArr.push({ timestamp: new Date(dateString1), price: Math.round((priceQuantitySum / quantSum + Number.EPSILON) * 100) / 100 });
       res.json(finalArr);
     } else {
       res.json([]);
@@ -94,6 +94,5 @@ utilRouter.get("/order-history", async (req, res, next) => {
     next(e);
   }
 });
-
 
 export default utilRouter;
