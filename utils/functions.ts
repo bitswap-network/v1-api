407477f057c920a03299b54b167c88d1ebbdac87
Aryan Misra <aryanmisra4@gmail.com>
2021-06-29 21:05:42 -0400

    schema

--- utils/functions.ts
+++ utils/functions.ts
@@ -130,7 +130,6 @@ export const generateCode = (len: number) => [...Array(len)].map(() => Math.floo
 
 export const generateAccessToken = (PublicKeyBase58Check: any) => {
   return jwt.sign(PublicKeyBase58Check, config.JWT_KEY, { expiresIn: "18000s" });
-
 };
 
 export const generateHMAC = (body: any) => {
